6144be2272b919298f1fba08c806c1cd61ee410a
Oleg Polyakov <opolyo01@yahoo.com>
2024-01-09 22:24:34 -0600

    default route could be off

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,40 @@
-// App.tsx
-
-import { useState } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
 import AdminUpload from './AdminUpload';
 import './App.css';
 import HomePage from './HomePage';
 import Tab from './Tab';
 
 function App(): JSX.Element {
-  const [selectedTab, setSelectedTab] = useState('/');
-
   return (
     <BrowserRouter>
-      <div className="tab-container">
-        <Tab
-          to="/"
-          selected={selectedTab === '/'}
-          onClick={() => setSelectedTab('/')}
-        >
-          Home
-        </Tab>
-        <Tab
-          to="/admin"
-          selected={selectedTab === '/admin'}
-          onClick={() => setSelectedTab('/admin')}
-        >
-          Admin
-        </Tab>
-        {/* Define routes inside the Routes component */}
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/admin" element={<AdminUpload />} />
-        </Routes>
-      </div>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/admin" element={<AdminUpload />} />
+      </Routes>
     </BrowserRouter>
   );
 }
+
+const Navigation = () => {
+  const location = useLocation();
+  const [selectedTab, setSelectedTab] = useState(location.pathname);
+
+  useEffect(() => {
+    setSelectedTab(location.pathname);
+  }, [location]);
+
+  return (
+    <div className="tab-container">
+      <Tab to="/" selected={selectedTab === '/'} onClick={() => setSelectedTab('/')}>
+        Home
+      </Tab>
+      <Tab to="/admin" selected={selectedTab === '/admin'} onClick={() => setSelectedTab('/admin')}>
+        Admin
+      </Tab>
+    </div>
+  );
+};
 
 export default App;
